3756ae66d5593809d3fbc7e44505fc8ba8e37e12
annapoornavh <annapoorna.xworkz@gmail.com>
2024-07-23 15:08:02 +0530

    upload

---
+++ methods/AlienRunner.java
@@ -0,0 +1,9 @@
+class AlienRunner
+{
+	public static void main(String[]args)
+	{
+		System.out.println("start AlienRunner");
+		new Alien();
+		System.out.println("end ALienRunner");
+	}
+}
---
+++ methods/HelicopterRunner.java
@@ -0,0 +1,62 @@
+class HelicopterRunner
+{
+	public static void main(String[] args)
+	{
+		System.out.println("start main Runner");
+		new Helicopter();
+		new Helicopter();
+		new Market();
+		Spoon anything2=new Spoon();
+		new Market();
+		new Train();
+		new Mall();
+		Helicopter helicopter=new Helicopter();
+		Market market1=new Market();
+		Mall mall=new Mall();
+		new Market();
+        Mall mall1=new Mall();
+        mall=mall1;
+        new Wrapper();
+        Wrapper wrapper=new Wrapper();
+        new Ring();
+		new Auditorium();
+        new Ring();
+		new Ring();
+		Auto auto=new Auto();
+		Auto auto1=new Auto();
+		Auto auto2=new Auto();
+		auto1=auto2;
+		new Auditorium();
+		new Auditorium();
+		new BlackHole();
+		new Auditorium();
+		new Auditorium();
+		BlackHole blackHole=new BlackHole();
+		new Auditorium();
+		SpaceShip spaceShip=new SpaceShip();	
+        SpaceShip anything=new SpaceShip();	
+        SpaceShip market=new SpaceShip();	
+		new Bullet();
+		Bullet bullet=new Bullet();
+		new Galaxy();
+		Galaxy galaxy=new Galaxy();
+		Spoon spoon=new Spoon();
+		Spoon anything1=new Spoon();
+		new Cake();
+		Cake cake=new Cake();
+		Cake cake1=new Cake();
+		Cake cake2=new Cake();
+		Cake cake3=new Cake();
+		cake=cake1=cake2=cake3;
+		
+		
+		
+		
+		
+		
+        		
+		
+		System.out.println("end main Runner");
+		
+	}
+}
