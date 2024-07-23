9f114c8c6191c8cdb51f59dff6efdcff222461f1
annapoornavh <annapoorna.xworkz@gmail.com>
2024-07-23 15:13:44 +0530

    upload

---
+++ methods/Constructor/Helicopter.java
@@ -0,0 +1,125 @@
+class Helicopter
+{
+	public Helicopter()
+	{
+		System.out.println("Created Helicopter");
+	}
+}
+
+
+class Market
+{
+	public Market()
+	{
+		System.out.println("Market Created");
+	}
+}
+
+
+class Train
+{
+	Train()
+	{
+		System.out.println("Train is already running");
+	}
+}
+
+
+class Mall
+{
+	public Mall()
+	{
+		System.out.println("Mall is open");
+	}
+}
+
+
+class Wrapper
+{
+	Wrapper()
+	{
+		System.out.println("Wrapper is use to pack");
+	}
+}
+
+
+class Ring
+{
+	public Ring()
+	{
+		System.out.println("Ring proposal");
+	}
+}
+
+
+class Auto
+{
+	public Auto()
+	{
+		System.out.println("Auto stand is created");
+	}
+}
+
+
+class Auditorium
+{
+	Auditorium()
+	{
+		System.out.println("Auditorium Hall");
+	}
+}
+
+
+class SpaceShip
+{
+	SpaceShip()
+	{
+		System.out.println("SpaceShip is created");
+	}
+}
+
+
+class Bullet
+{
+	public Bullet()
+	{
+		System.out.println("Bullet shooting");
+	}
+}
+
+
+class Galaxy
+{
+	Galaxy()
+	{
+		System.out.println("Created Galaxy");
+		
+	}
+}
+
+
+class Spoon
+{
+	public Spoon()
+	{
+		System.out.println("Spoon to eat");
+	}
+}
+
+
+class BlackHole
+{
+	public BlackHole()
+	{
+		System.out.println("Created BlackHole");
+	}
+}
+
+
+class Cake
+{
+	Cake()
+	{
+		System.out.println("Cake is soo sweet");
+	}
+}
